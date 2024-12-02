ea859a4ab5a39169142c735b324b1624a8ffe560
Justice Oyakhire <justiceoyakhire27@gmail.com>
2024-12-02 17:22:24 +0100

    created a simple server

---
+++ server.js
@@ -0,0 +1,40 @@
+import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+const PORT = 2000;
+
+// Get path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//create server with node
+
+const server = http.createServer(async (req, res) => {
+  try {
+    //check if GET request
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/contact") {
+        filePath = path.join(__dirname, "public", "contact.html");
+      } else {
+        throw new Error("Not Found");
+      }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.end("<h1>Not Found  </h1>");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
